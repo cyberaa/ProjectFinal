7d7900ec30b6a0f2bfcd4598bde5b8a71ff0cdda
joaosimoes <joaomiguel_condeixa@hotmail.com>
2013-10-19 21:43:04 +0100

    Creation of Client class.

---
+++ src/client/Client.java
@@ -0,0 +1,14 @@
+package client;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: joaosimoes
+ * Date: 10/19/13
+ * Time: 9:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Client {
+
+
+
+}
