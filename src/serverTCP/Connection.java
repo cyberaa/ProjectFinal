e89bd573fe14de4b1d3f9254031513e3ac374ac8
ThrostGunnulf <joaonunoao@gmail.com>
2013-10-20 15:29:36 +0100

    Implemented lookupObjects().

--- src/serverTCP/Connection.java
+++ src/serverTCP/Connection.java
@@ -1,9 +1,19 @@
 package serverTCP;
 
+import common.rmi.RemoteIdeas;
+import common.rmi.RemoteTopics;
+import common.rmi.RemoteTransactions;
+import common.rmi.RemoteUserManager;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.MalformedURLException;
 import java.net.Socket;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 /**
  * Created with IntelliJ IDEA.
  * User: joaosimoes
@@ -11,28 +21,55 @@ import java.net.Socket;
  * Time: 4:44 PM
  * To change this template use File | Settings | File Templates.
  */
-public class Connection extends Thread {
+public class Connection extends Thread
+{
+	protected Socket clientSocket;
+	protected ObjectInputStream inStream;
+	protected ObjectOutputStream outStream;
 
-    private boolean shutDown;
-    private Socket clientSocket;
-    private ObjectInputStream inStream;
-    private ObjectOutputStream outStream;
+	//Remote objects.
+	protected RemoteUserManager um;
+	protected RemoteIdeas ideas;
+	protected RemoteTopics topics;
+	protected RemoteTransactions transactions;
 
-
-    public Connection(Socket cSocket) {
+    public Connection(Socket cSocket)
+    {
         clientSocket = cSocket;
-        shutDown = false;
+
         try {
 	        outStream = new ObjectOutputStream(cSocket.getOutputStream());
             inStream = new ObjectInputStream(cSocket.getInputStream());
-
         } catch (IOException ie) {
-	        System.out.println("Could not create connection:\n" + ie);
+	        System.out.println("Could not create input and output streams:\n" + ie);
         }
     }
 
     @Override
-    public void run() {
+    public void run()
+    {
 
     }
+
+	/**
+	 * Lookup the remote objects and save them to class variables.
+	 * @throws RemoteException
+	 */
+	protected void lookupRemotes() throws RemoteException
+	{
+		String rmiAddress = "rmi://"+ServerTCP.rmiServerAddress+":"+ServerTCP.rmiRegistryPort+"/";
+
+		try {
+			um = (RemoteUserManager) Naming.lookup(rmiAddress+"UserManager");
+			ideas = (RemoteIdeas) Naming.lookup(rmiAddress+"Ideas");
+			topics = (RemoteTopics) Naming.lookup(rmiAddress+"Topics");
+			transactions = (RemoteTransactions) Naming.lookup(rmiAddress+"Transactions");
+		} catch (MalformedURLException mue) {
+			System.out.println("Wrong URL passed as argument:\n" + mue);
+			System.exit(-1);
+		} catch (NotBoundException nbe) {
+			System.out.println("Object is not bound:\n" + nbe);
+			System.exit(-1);
+		}
+	}
 }
