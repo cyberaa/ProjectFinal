358b0b37639408a22afe9741a03ee80ce89b4888
joaosimoes <joaomiguel_condeixa@hotmail.com>
2013-10-20 19:01:59 +0100

    Test Login

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,6 +1,8 @@
 package client;
 
+import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,8 +16,38 @@ public class Client {
     protected static Socket s;
     protected static String serverAddress_1;
     protected static String serverAddress_2;
-    protected static String serverPort;
+    protected static int serverPort1;
+    protected static int serverPort2;
 
+    public static void main(String args[]) {
 
+        if(args.length != 4)
+        {
+            System.out.println("Usage: java ServerTCP <server1_address> <server1_port> <server2_address> <server2_port>");
+            return;
+        }
+
+        serverAddress_1 = args[0];
+        serverPort1 = Integer.parseInt(args[1]);
+
+        serverAddress_2 = args[2];
+        serverPort2 = Integer.parseInt(args[3]);
+
+        try {
+            s = new Socket(serverAddress_1, serverPort1);
+        } catch(IOException ioe) {
+            System.out.println("Error in socket creation.\n" + ioe);
+        }
+
+        String username;
+        String password;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Username: ");
+        username = sc.next();
+        System.out.print("Password: ");
+        password = sc.next();
+
+    }
 
 }
