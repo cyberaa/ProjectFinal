ee62867a4c0d0f9f7ba2f61092d5df02ba2730b0
ThrostGunnulf <joaonunoao@gmail.com>
2013-10-20 20:57:19 +0100

    Updated SetShareValue.java

--- src/common/tcp/SetShareValue.java
+++ src/common/tcp/SetShareValue.java
@@ -13,10 +13,13 @@ public class SetShareValue implements Serializable {
 
 	private static final long serialVersionUID = 6208565993764399537L;
 
+	public int user_id;
 	public int share_id;
     public int new_value;
 
-    public SetShareValue(int share_id, int new_value) {
+    public SetShareValue(int user_id, int share_id, int new_value)
+    {
+	    this.user_id = user_id;
         this.share_id = share_id;
         this.new_value = new_value;
     }
