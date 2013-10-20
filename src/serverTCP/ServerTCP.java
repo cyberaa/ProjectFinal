2ff5621280af9eb24b63b8ec9e58abbfc39ae71b
ThrostGunnulf <joaonunoao@gmail.com>
2013-10-20 21:53:29 +0100

    Created a new socket in ServerTCP to accept connections for notifications.

--- src/serverTCP/ServerTCP.java
+++ src/serverTCP/ServerTCP.java
@@ -4,7 +4,7 @@ package serverTCP;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.rmi.RMISecurityManager;
+import java.net.SocketTimeoutException;
 
 /**
  * Created with IntelliJ IDEA.
@@ -20,44 +20,66 @@ public class ServerTCP {
     protected static int rmiRegistryPort;
 
     //TCP Connection
-    protected static int serverPort;
-    protected static ServerSocket listenSocket;
+    protected static int conPort;
+    protected static ServerSocket conListenSocket;
+	protected static int notPort;
+	protected static ServerSocket notListenSocket;
 
-
+	private static final int timeout = 10;
 
     public static void main(String args[]) {
         //Verify the number of given arguments.
-        if(args.length != 3)
+        if(args.length != 4)
         {
-            System.out.println("Usage: java ServerTCP <server_port> <rmi_registry_port> <rmi_registry_address>");
+            System.out.println("Usage: java ServerTCP <server_connection_port> <server_notify_port> <rmi_registry_port> <rmi_registry_address>");
             return;
         }
 
 	    //Get command line arguments.
-        serverPort = Integer.parseInt(args[0]);
-        rmiServerAddress = args[2];
-        rmiRegistryPort = Integer.parseInt(args[1]);
+        conPort = Integer.parseInt(args[0]);
+	    notPort = Integer.parseInt(args[1]);
+        rmiServerAddress = args[3];
+        rmiRegistryPort = Integer.parseInt(args[2]);
 
 	    //Set system policies.
 	    //System.getProperties().put("java.security.policy", "policy.all");
 	    //System.setSecurityManager(new RMISecurityManager());
 
         try {
-            listenSocket = new ServerSocket(serverPort);
+            conListenSocket = new ServerSocket(conPort);
+	        conListenSocket.setSoTimeout(timeout);
+
+	        notListenSocket = new ServerSocket(notPort);
+	        notListenSocket.setSoTimeout(timeout);
         } catch (IOException ie) {
             System.out.println("Error in server socket creation.\n"+ ie);
         }
 
         Socket s;
 
-        while (true) {
+        while (true)
+        {
             try {
-                s = listenSocket.accept();
+                s = conListenSocket.accept();
                 new Connection(s);
+            } catch (SocketTimeoutException e) {
+	            //Do nothing.
             } catch (IOException e) {
-                System.out.print("LOL" + e);
-                return;
+	            System.out.print("Connection listen socket error:\n" + e);
+	            return;
             }
+
+	        try {
+		        s = notListenSocket.accept();
+		        //new Notifications(s); //TODO: implement thread to deal with notifications.
+	        } catch (SocketTimeoutException e) {
+		        //Do nothing.
+	        } catch (IOException e) {
+		        System.out.print("Connection listen socket error:\n" + e);
+		        return;
+	        }
+
+	        //TODO: UDP fail-over stuff.
         }
     }
 }
